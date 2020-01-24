754c794176b1527e6b936a019efaa28f7f4d1594
Zach <zpman95@hotmail.com>
2020-01-24 12:46:38 -0600

    completed robot

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,6 +97,25 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
+        self.set_light_on()
+        while self.light_is_on():
+            self.set_light_off()
+            while self.can_move_right():
+                self.swap_item()
+                self.move_right()
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                    self.set_light_on()
+                else:
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+            if self.light_is_on():
+                while self.can_move_left():
+                    self.move_left()
         pass
 
 
